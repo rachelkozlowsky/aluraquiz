55e80af9ab778e89185e8b20df9a1bd47f0f7b9e
Rachel Kozlowsky <rachelkozlowsky@gmail.com>
2021-01-28 19:51:28 -0300

    atualização aulas 2,3,4 Imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,73 @@
+import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
-import Widget from '../src/components/Widget'
+import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
-const QuizContainer = styled.div`
-width: 100%;
-max-width: 350px;
-padding-top: 45px;
-margin: auto 10%;
-@media screen and (max-width: 500px) {
-  margin: auto;
-  padding: 15px;
-}
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
-        <Widget.Header>
-              <h1>Musica</h1>
-              </Widget.Header>
+          <Widget.Header>
+            <h1>Música Popular Brasileira</h1>
+          </Widget.Header>
           <Widget.Content>
-          <p>lorem ipsum dolor sit amet...</p>
+            {/* eslint-disable-next-line func-names */}
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Seu nome"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
-       
+
         <Widget>
-            
           <Widget.Content>
-          <h1>Quizes da Galera</h1>
-          <p>lorem ipsum dolor sit amet...</p>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
-       <Footer />
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl= "https://github.com/rachelkozlowsky" />
+      <GitHubCorner projectUrl="https://github.com/rachelkozlowsky" />
     </QuizBackground>
-  
-  )
+  );
 }
